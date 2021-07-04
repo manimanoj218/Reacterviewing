6e4b0ea386b908f0306f4c12ddaecf9314184f41
manoj reddy <manimanoj218@gmail.com>
2021-07-04 21:57:04 +0530

    bug fix in the albumlist and api for to get album lists

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { hot } from "react-hot-loader";
 import AlbumList from "./album/AlbumList";
 import "./App.css";
 
-const App = () => (
+const App = () => {
+  const [albumsList, setAlbumList] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((res) => res.json())
+      .then((data) => {
+        let modifiedData = data.map((album) => {
+          return {
+            ...album,
+            title: album.title.replaceAll(/e/gi, ""),
+          };
+        });
+        setAlbumList(modifiedData);
+      });
+  }, []);
+
+  return (
     <div className="albums-app">
-        <h1>React Interview Challenge</h1>
-        <AlbumList />
+      <h1>React Interview Challenge</h1>
+      <AlbumList albums={albumsList} />
     </div>
-);
+  );
+};
 
 export default hot(module)(App);
--- src/album/AlbumItem.js
+++ src/album/AlbumItem.js
@@ -3,10 +3,10 @@ import placeholder from "./placeholder.svg";
 import "./AlbumItem.css";
 
 const AlbumItem = ({ album }) => (
-    <li className="album-item">
-        <img src={placeholder}></img>
-        <h3>{album.title}</h3>
-    </li>
+  <li className="album-item">
+    <img src={album.url ? album.url : placeholder}></img>
+    <h3>{album.title}</h3>
+  </li>
 );
 
 export default AlbumItem;
